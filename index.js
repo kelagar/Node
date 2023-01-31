7f91ade016bcef7de90b44decda9c21e0d96f27b
Kelagar <kelagar123@gmail.com>
2023-01-31 16:20:48 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json("Hello World!");
+});
+
+//Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/companies", require("./routes/api/companies"));
+app.use("/api/login", require("./routes/api/login"));
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
